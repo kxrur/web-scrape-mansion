e661dc38655742c7f93eadf9d292870a05e0b59a
kxrury <kxrury@gmail.com>
2024-07-16 22:25:38 -0400

    setup for Tauri app dev

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { invoke } from '@tauri-apps/api'
+
+invoke('greet', { name: 'World' })
+  // `invoke` returns a Promise
+  .then((response) => console.log(response))
+</script>
+
+<template>
+  <div>
+    <Button>Click me</Button>
+  </div>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import path from 'node:path'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
+export default defineConfig({
+
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  // prevent vite from obscuring rust errors
+  clearScreen: false,
+  // Tauri expects a fixed port, fail if that port is not available
+  server: {
+    strictPort: true,
+    port: 5173,
+  },
+  // to access the Tauri environment variables set by the CLI with information about the current target
+  envPrefix: ['VITE_', 'TAURI_PLATFORM', 'TAURI_ARCH', 'TAURI_FAMILY', 'TAURI_PLATFORM_VERSION', 'TAURI_PLATFORM_TYPE', 'TAURI_DEBUG'],
+  build: {
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
+})
