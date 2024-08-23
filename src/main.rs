941fbbc75c7e5f9f278e4fd580abe4fd8f6d4324
kxrury <kxrury@gmail.com>
2024-08-23 00:02:38 -0400

    scrape the number of available rooms

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use crate::links::extract_savills_urls;
 
 mod scrape;
 use crate::scrape::scrape::{
-    eval_address, eval_images, eval_price, eval_size, ADDRESS1_CS, ADDRESS2_CS,
+    eval_address, eval_images, eval_price, eval_room, eval_size, ADDRESS1_CS, ADDRESS2_CS,
 };
 
 mod database;
@@ -20,7 +20,7 @@ use crate::database::sql::{establish_pool, pull, push, some_mansions};
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     dotenv().ok();
-    let pool = establish_pool();
+    let _pool = establish_pool();
 
     let file_path = "bookmarks.html";
     let all_links = extract_savills_urls(file_path);
@@ -31,7 +31,7 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
 
     match all_links {
         Ok(urls) => {
-            let mut caps = DesiredCapabilities::chrome();
+            let caps = DesiredCapabilities::chrome();
             //caps.add_arg("--headless=new")?; // hide the browser
             let driver = WebDriver::new("http://localhost:9515", caps).await?;
             for url in urls {
@@ -42,14 +42,16 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
                     let address2 = eval_address(&driver, ADDRESS2_CS).await?;
                     let full_address = format!("{} {}", address1, address2);
                     println!("full address: {}", full_address);
-                    let price = match eval_price(&driver).await {
+                    let _price = match eval_price(&driver).await {
                         Ok(it) => Some(it),
                         Err(_) => {
                             println!("No price found");
                             None
                         }
-                    }; // is an Option<i32> so gotta unwrap when using
-                    let size = eval_size(&driver).await?;
+                    }; // is an Option<i32> so gotta extract when using
+                    let _size = eval_size(&driver).await?;
+
+                    let (_bedrooms, _bathrooms, _receptions) = eval_room(&driver).await?; //option,
 
                     if let Ok(elem_cookie_block) = driver
                         .find(By::ClassName("sv-cookie-management__banner-cta"))
--- src/scrape/scrape.rs
+++ src/scrape/scrape.rs
@@ -9,6 +9,7 @@ pub const ADDRESS1_CS: &str = "sv-property-intro__address-line-1";
 pub const ADDRESS2_CS: &str = "sv-property-intro__address-line-2";
 pub const PRICE_CS: &str = "sv-property-price__wrap:nth-child(2) > span:nth-child(3)";
 pub const SIZE_CS: &str = "sv--size > span:nth-child(1)";
+pub const ROOMS_CS: &str = "sv-property-intro-footer__group:nth-child(2)";
 
 pub async fn eval_images(
     elem_block: WebElement,
@@ -51,21 +52,58 @@ pub async fn eval_price(driver: &WebDriver) -> Result<i32, WebDriverError> {
 
     let price: i32 = price_str.parse().unwrap();
 
-    println!("price: {}\n end", price);
+    println!("price: {}", price);
     Ok(price)
 }
 
-pub async fn eval_size(driver: &WebDriver) -> Result<i32, WebDriverError> {
+pub async fn eval_size(driver: &WebDriver) -> Result<f64, WebDriverError> {
     let elem_size = driver.find(By::ClassName(SIZE_CS)).await?;
     let mut size_str = elem_size.text().await?;
     size_str.retain(|c| c != ',');
 
-    let re = Regex::new(r"(.*)\((?<size>[0-9.]*)(.*)").unwrap();
+    let re = Regex::new(r"(.*)\((?<size>[0-9\.]*)(.*)").unwrap();
     let caps = re.captures(&size_str).unwrap();
     size_str = caps["size"].to_string();
 
-    let size: i32 = size_str.parse().unwrap();
+    let size: f64 = size_str.parse().unwrap();
 
     println!("size: {} sq m", size_str);
     Ok(size)
+}
+
+pub async fn eval_room(
+    driver: &WebDriver,
+) -> Result<(Option<i32>, Option<i32>, Option<i32>), WebDriverError> {
+    let (mut room, mut bath, mut rec): (Option<i32>, Option<i32>, Option<i32>) = (None, None, None);
+    let elem_room = driver.find_all(By::ClassName(ROOMS_CS)).await?;
+
+    for roomie_cat in elem_room.iter() {
+        let roomies = roomie_cat.find_all(By::Tag("div")).await?;
+
+        for roomie in roomies {
+            let roomie_span = roomie.find_all(By::Tag("span")).await?;
+            let label = roomie_span[1].text().await?;
+            let _: Option<i32> = match roomie_span[0].text().await?.parse() {
+                Ok(it) => {
+                    match label.as_str() {
+                        "Bedrooms" => room = Some(it),
+                        "Bathrooms" => bath = Some(it),
+                        "Receptions" => rec = Some(it),
+                        _ => (),
+                    };
+                    Some(it)
+                }
+                Err(e) => {
+                    eprint!("{}", e);
+                    None
+                }
+            };
+        }
+    }
+
+    println!(
+        "Bedrooms: {:?}\nBathrooms: {:?}\nReceptions: {:?}",
+        room, bath, rec
+    );
+    Ok((room, bath, rec))
 }
