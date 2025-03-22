f386fd212a33ab45d12753dcdda09e5ee84dec1b
kxrur <kxrury@gmail.com>
2025-03-22 14:30:37 -0400

    feat(vue-devtools): add port and dependencie
    used to debug frontend (looking into the 'Apply' button's emits

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -8,13 +8,15 @@
 import vuetify from './vuetify'
 import pinia from '../stores'
 import router from '../router'
+import { devtools } from '@vue/devtools'
+
+if (process.env.NODE_ENV === 'development') {
+  devtools.connect()
+}
 
 // Types
 import type { App } from 'vue'
 
-export function registerPlugins (app: App) {
-  app
-    .use(vuetify)
-    .use(router)
-    .use(pinia)
+export function registerPlugins(app: App) {
+  app.use(vuetify).use(router).use(pinia)
 }
