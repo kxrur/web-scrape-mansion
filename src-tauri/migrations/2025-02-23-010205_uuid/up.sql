f2fffd961e0f95b7b28ba9a0fcf974828c48ff6a
kxrur <kxrury@gmail.com>
2025-03-29 17:57:34 -0400

    refactor(psql->sqlite): remove all psql database
    BREAKING CHANGE: no database is in place

--- src-tauri/migrations/2025-02-23-010205_uuid/up.sql
+++
@@ -1,2 +0,0 @@
--- Your SQL goes here
-ALTER TABLE mansionees ADD COLUMN uuid UUID DEFAULT gen_random_uuid() NOT NULL;
--- src-tauri/src/database/postgresql.rs
+++ src-tauri/src/database/postgresql.rs
@@ -1,4 +1,4 @@
-use diesel::{Connection, PgConnection};
+use diesel::{Connection, SqliteConnection};
 use dotenv::dotenv;
 use std::{env, panic, sync::Mutex};
 
@@ -17,7 +17,7 @@ pub fn establish_connection() -> PgConnection {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url)
+    SqliteConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
 pub fn save_mansionee_to_database(new_mansion: NewMansionee) -> Mansionee {
