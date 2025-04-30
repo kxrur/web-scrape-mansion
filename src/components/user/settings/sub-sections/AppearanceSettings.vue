7d431156c572f403c4da29aff0d319a71c82da9e
kxrur <kxrury@gmail.com>
2025-04-30 00:39:39 -0400

    feat(theme): select theme in the settings
    select one of the defined themes in 'customThemes'
    
    BREAKING CHANGE: theme is now loaded from the database on app launch - defaults to system

--- src/components/user/settings/sub-sections/AppearanceSettings.vue
+++ src/components/user/settings/sub-sections/AppearanceSettings.vue
@@ -4,10 +4,12 @@
 
     <v-select
       label="Theme"
-      :items="['Light', 'Dark', 'System']"
+      :items="themeOptions"
       variant="outlined"
       class="mb-4"
       color="primary"
+      v-model="selectedTheme"
+      @update:modelValue="changeTheme"
     ></v-select>
 
     <v-switch
@@ -19,4 +21,56 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useTheme } from 'vuetify'
+import { customThemes } from '@/plugins/CustomThemes'
+
+const theme = useTheme()
+const selectedTheme = ref('System')
+
+interface ThemeOption {
+  title: string
+  value: string
+}
+
+const customThemeNames: ThemeOption[] = Object.keys(customThemes).map(
+  (name) => ({
+    title: name.replace(/([A-Z])/g, ' $1').trim(),
+    value: name,
+  })
+)
+
+// Available theme options
+const themeOptions: ThemeOption[] = [
+  { title: 'Light', value: 'light' },
+  { title: 'Dark', value: 'dark' },
+  { title: 'System', value: 'system' },
+  ...customThemeNames,
+]
+
+console.log(themeOptions)
+
+function changeTheme(newTheme: string) {
+  switch (newTheme) {
+    case 'light':
+    case 'dark':
+      theme.global.name.value = newTheme
+      break
+    case 'system':
+      const prefersDark = window.matchMedia(
+        '(prefers-color-scheme: dark)'
+      ).matches
+      theme.global.name.value = prefersDark ? 'dark' : 'light'
+      break
+    default:
+      if (customThemes[newTheme]) {
+        theme.global.name.value = newTheme
+      }
+  }
+}
+
+onMounted(() => {
+  selectedTheme.value = 'system'
+  changeTheme('system')
+})
+</script>
